c8cf4796df04812a23b9ba91cd36b51623b97b98
KooTaehyeon <vcro12123@naver.com>
2023-03-29 23:07:19 +0900

    ts 설정

--- build/index.js
+++ build/index.js
@@ -1,8 +1,16 @@
-class Block {
-    constructor(data) {
-        this.data = data;
-    }
-    static hello() {
-        return 'hi';
-    }
-}
+"use strict";
+//import { init, exit } from './myPackage';
+// class Block {
+//   constructor(private data: string) {}
+//   static hello() {
+//     return 'hi';
+//   }
+// }
+// Math.fround();
+// init({
+//   url: 'true',
+// });
+// exit(1);
+// localStorage.clear();
+// init({ url: 'http', debug: true });
+console.log('hi');
--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,17 @@
 
 // localStorage.clear();
 // init({ url: 'http', debug: true });
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {}
+}
